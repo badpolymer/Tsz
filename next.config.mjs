7002a689431d18ea47cb5fce4a3eeac62d0dbe94
badpolymer <roans_policy.07@icloud.com>
2024-02-22 22:02:45 -0500

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     assetPrefix: isProd ? 'https://badpolymer.github.io/Tsz' : undefined,
+    basePath: '/Tsz',
     experimental: {}
 };
 
