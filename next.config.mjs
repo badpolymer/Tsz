f22d6de22f56dc122fc5d6eacac3e606dac0ea3b
badpolymer <roans_policy.07@icloud.com>
2024-02-29 01:11:21 -0500

    temp config

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,10 @@ const nextConfig = {
     output: 'export',
     assetPrefix: isProd ? 'https://badpolymer.github.io/Tsz' : undefined,
     basePath: isProd ? '/Tsz' : undefined,
-    experimental: {}
+    experimental: {
+        missingSuspenseWithCSRBailout: false
+        //https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
+    }
 };
 
 export default nextConfig;
