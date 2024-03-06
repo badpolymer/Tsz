5f02273a6c0686cf240367010b231bb4642941ee
badpolymer <roans_policy.07@icloud.com>
2024-03-06 00:43:51 -0500

    ??

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,16 @@ import LanguageSelector from "./LanguageSelector";
 import ThemeSwitch from "./ThemeSwitch";
 
 export default function Header({
-   
-}:{
-   
-}) {
+
+}: {
+
+    }) {
     return (
         <header>
             <Suspense>
                 <LanguageSelector></LanguageSelector>
+            </Suspense>
+            <Suspense>
                 <ThemeSwitch></ThemeSwitch>
             </Suspense>
 
