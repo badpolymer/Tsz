bf7d70115bb63355188a567d4229fab59534f5ac
badpolymer <roans_policy.07@icloud.com>
2024-03-06 00:35:06 -0500

    59:8  Warning: React Hook useEffect has a missing dependency: 'toggleID'. Either include it or remove the dependency array.  react-hooks/exhaustive-deps

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -56,7 +56,7 @@ export default function ThemeSwitch({
                     break;
             }
         }
-    }, []);
+    }, [toggleID]);
 
     if (!localStorageAvailable) {
         return null;
