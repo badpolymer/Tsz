fec8d8321e1fe991618cace2caaf71aacca8d02b
badpolymer <roans_policy.07@icloud.com>
2024-05-27 00:20:25 -0400

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,65 @@ import "./globals.scss";
 //const noto = Noto_Serif_HK({ preload:false});
 //https://github.com/vercel/next.js/discussions/47309
 
+const isProd = process.env.NODE_ENV === 'production';
+
+
 export const metadata: Metadata = {
+  metadataBase: isProd ? new URL('https://badpolymer.github.io/Tsz/') : new URL(`http://localhost:${process.env.PORT || 3000}`),
+  alternates: {
+    canonical: `/`,
+    languages: {
+      'en-Ca': `/en-ca`,
+      'en-US': `en-us`,
+      'en-GB': `/en-uk`,
+      'zh-HK': `/ct`,
+      'zh-TW': `/mdr`,
+      'zh': `/ct`,
+    },
+  },
   title: {
     template: '%s - 匯典 Tsz',
     default: '匯典 Tsz', // a default is required when creating a template
   },
+  description: ``,
+  keywords: [`Cantonese`, `Dictionary`, `辭典`],
+  openGraph: {
+    type: "website",
+    url: "https://badpolymer.github.io/Tsz/",
+    title: "匯典 Tsz",
+    description: "My Website Description",
+    siteName: "匯典 Tsz",
+    images: [{
+      url: "/favicon.ico",
+    }],
+    determiner: 'auto',
+    emails: `string | Array<string>`,
+    phoneNumbers: [`+1 6329545648`],
+    faxNumbers: `string | Array<string>;`,
+    locale: `Locale`,
+    alternateLocale: [``],
+    audio: `OGAudio | Array<OGAudio>`,
+    videos: `OGVideo | Array<OGVideo>;`,
+    countryName: `string`,
+    ttl: 123,
+  },
+  twitter: {
+    site: `string`,
+    siteId: `string`,
+    creator: `string`,
+    creatorId: `string`,
+    description: `string`,
+    title: `string`,
+    images: `TwitterImage | Array<TwitterImage>`,
+  },
+  formatDetection: {
+    telephone: false,
+    date: false,
+    address: false,
+    email: false,
+    url: false,
+  }
+
 }
 
 export default function RootLayout({
@@ -23,7 +77,7 @@ export default function RootLayout({
     <html>
       {/* <body className={noto.className}> */}
       {/* <body className={myFont.className}> */}
-          {children}
+      {children}
       {/* </body> */}
     </html>
 
